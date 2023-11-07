6b5d26c4e403cd5e279e49e3b69dc041fca03940
raviz-es <raviz-es@student.42london.com>
2023-11-07 13:39:14 +0000

    commit

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,8 +6,26 @@
 /*   By: raviz-es <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 11:52:13 by raviz-es          #+#    #+#             */
-/*   Updated: 2023/11/07 12:34:29 by raviz-es         ###   ########.fr       */
+/*   Updated: 2023/11/07 13:33:26 by raviz-es         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include <stdio.h>
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1024);
+	else
+		return (0);
+}
+
+
+#include <unistd.h>
+#include <ctype.h>
+int	main(void)
+{
+	printf("%d", ft_isalpha('A'));
+	printf("\n%d", isalpha('B'));
+	return (0);
+}
