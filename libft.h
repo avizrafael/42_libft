6b42c1bbe0d6ddeeb27464923c73192b60747531
raviz-es <raviz-es@student.42london.com>
2023-11-07 21:06:46 +0000

    commit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: raviz-es <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 18:08:56 by raviz-es          #+#    #+#             */
-/*   Updated: 2023/11/07 20:58:53 by raviz-es         ###   ########.fr       */
+/*   Updated: 2023/11/07 21:04:36 by raviz-es         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,36 @@ int ft_isascii(int c);
 int ft_isdigit(int c);
 int ft_isprint(int c);
 
-strlen
-memset
-bzero
-memcpy
-memmove
-strlcpy
-strlcat
-toupper
-tolower
-strchr
-strrchr
-strncmp
-memchr
+ft_strlen
+ft_memset
+ft_bzero
+ft_memcpy
+ft_memmove
+ft_strlcpy
+ft_strlcat
+ft_toupper
+ft_tolower
+ft_strchr
+ft_strrchr
+ft_strncmp
+ft_memchr
+ft_memcmp
+ft_strnstr
+ft_atoi
+ft_calloc
+ft_strdup
+
+char *ft_substr(char const *s, unsigned int start, size_t len);
+char *ft_strjoin(char const *s1, char const *s2);
+char *ft_strtrim(char const *s1, char const *set);
+char **ft_split(char const *s, char c);
+char *ft_itoa(int n);
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void ft_striteri(char *s, void (*f)(unsigned int, char*));
+void ft_putchar_fd(char c, int fd);
+void ft_putstr_fd(char *s, int fd);
+void ft_putendl_fd(char *s, int fd);
+void ft_putnbr_fd(int n, int fd);
+
 
 #endif
